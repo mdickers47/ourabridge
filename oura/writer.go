e4b4de855a5b937013453b7cc783760b37393eb5
Mikey Dickerson <pomonamikey@gmail.com>
2024-08-09 13:59:07 -0700

    reconnect rather than die when graphite server goes away

--- oura/writer.go
+++ oura/writer.go
@@ -16,11 +16,22 @@ type Observation struct {
 	Value     float32
 }
 
+// StoreObservations uses the LocalDataLog, GraphiteServer, and
+// GraphitePrefix entries in cfg.  It tries to os.OpenFile the path in
+// cfg.LocalDataLog, and tries to net.Dial the address in
+// cfg.GraphiteServer.  It closes and reopens them both every 15
+// minutes.  When there is a write error to the local file, or when
+// cfg.Reconnect == true, it tries to reopen both immediately.  When
+// there is a write error to graphite, it lets the 15-minly reconnect
+// try again.  If at any time neither LocalLogFile nor GraphiteServer
+// is working, then the process dies with a log.Fatalf.
+
 func StoreObservations(cfg *ClientConfig, src chan Observation) {
 	var local *os.File
 	var graphite net.Conn
 	var have_local, have_graphite bool
 	var err error
+	var next_reconnect time.Time
 
 	close := func() {
 		if have_local {
@@ -40,7 +51,7 @@ func StoreObservations(cfg *ClientConfig, src chan Observation) {
 			if err != nil {
 				log.Printf("can't open log file: %s", err)
 			} else {
-				log.Printf("observations logged to local file %s", cfg.LocalDataLog)
+				log.Printf("opened local log file %s", cfg.LocalDataLog)
 				have_local = true
 			}
 		}
@@ -49,7 +60,7 @@ func StoreObservations(cfg *ClientConfig, src chan Observation) {
 			if err != nil {
 				log.Printf("can't connect to graphite server: %s", err)
 			} else {
-				log.Printf("observations going to graphite receiver at %s",
+				log.Printf("connected to graphite receiver at %s",
 					cfg.GraphiteServer)
 				have_graphite = true
 			}
@@ -63,10 +74,11 @@ func StoreObservations(cfg *ClientConfig, src chan Observation) {
 	defer close()
 	cfg.Reconnect = true
 	for obs := range src {
-		if cfg.Reconnect {
+		if cfg.Reconnect || time.Now().After(next_reconnect) {
 			close()
 			reconnect()
 			cfg.Reconnect = false
+			next_reconnect = time.Now().Add(15 * time.Minute)
 		}
 		line := fmt.Sprintf("%s%s.%s %f %d\n",
 			cfg.GraphitePrefix,
@@ -77,12 +89,21 @@ func StoreObservations(cfg *ClientConfig, src chan Observation) {
 		if have_local {
 			if _, err = io.WriteString(local, line); err != nil {
 				// if we are unable to record the observations, it is best to die
-				log.Fatalf("can't write to log file: %v", err)
+				log.Printf("failed write to log file: %s", err)
+				local.Close()
+				have_local = false
+				// it is a local file, so try to reopen it immediately
+				cfg.Reconnect = true
 			}
 		}
 		if have_graphite {
 			if _, err = io.WriteString(graphite, line); err != nil {
-				log.Fatalf("can't write to graphite server: %v", err)
+				log.Printf("failed write to graphite server: %s", err)
+				graphite.Close()
+				have_graphite = false
+				// we will try again at the next reconnect interval, rather
+				// than wait for a probably-hanging TCP connect attempt over
+				// and over
 			}
 		}
 	}
